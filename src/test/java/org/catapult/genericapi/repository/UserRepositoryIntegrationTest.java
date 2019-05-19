552e2443907749d783e702e08c4a23dc027aa9d3
Lam Do <lamdo@Lams-MacBook-Pro.local>
2019-05-19 15:16:54 +0700

    chore/dockerize the build

--- src/test/java/org/catapult/genericapi/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/org/catapult/genericapi/repository/UserRepositoryIntegrationTest.java
@@ -1,9 +1,12 @@
 package org.catapult.genericapi.repository;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 public class UserRepositoryIntegrationTest {
-
+	@Test
+	public void contextLoads() {
+	}
 }
